1f33dfd124ac9d6ce3300c1960942adf415f98ff
HuangKha097 <huangkha097@gmail.com>
2025-06-12 12:36:15 +0700

    Header#1

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 import './index.css';
 import App from './App.jsx';
 import GlobalStyles from './components/GlobalStyles';
+
+library.add(faCircleXmark, faMagnifyingGlass, faSpinner, faCoffee);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
