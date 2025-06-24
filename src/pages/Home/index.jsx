cab421b1912fb526d874a587cbe976a75c318bb2
HuangKha097 <huangkha097@gmail.com>
2025-06-24 11:46:43 +0700

    SideBar#4

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-<<<<<<< HEAD
     return (
         <div>
             <h2>home</h2>
         </div>
     );
-=======
-    return <h2>Home page</h2>;
->>>>>>> ab4c1c790ec81f48836a2381a4fc7201e13fecf0
 };
 
 export default Home;
